0c66268b868a2a4a37202aeca23924129421bf1e
jose@proint.com <jose@proint.com>
2024-09-01 16:24:45 +0000

    Edited homepage.clasesbienes

--- client_code/homepage/clasesbienes/__init__.py
+++ client_code/homepage/clasesbienes/__init__.py
@@ -17,7 +17,7 @@ class clasesbienes(clasesbienesTemplate):
   def refresh(self, **event_args):
     #self.repeating_panel_sucursales.items = anvil.server.call('get_Sucursales')
     self.repeating_panel_clasesbienes.items = anvil.server.call('get_clasesBienesSql')
-    
+    self.repeating_panel_clasesbienes.items = anvil.server.call
   def link_back_click(self, **event_args):
     """This method is called when the link is clicked"""
     open_form('homepage.mainmenu')
