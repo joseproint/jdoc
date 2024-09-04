2ebdd4ec82fd58584d714093385aafc5dc9d8bd1
jose@proint.com <jose@proint.com>
2024-09-04 21:23:12 +0000

    Edited homepage.markertaginfo

--- client_code/homepage/markertaginfo/__init__.py
+++ client_code/homepage/markertaginfo/__init__.py
@@ -1,10 +1,6 @@
 from ._anvil_designer import markertaginfoTemplate
 from anvil import *
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import stripe.checkout
-import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
