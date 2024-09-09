75582e354ef00654ca4ec927b5a2a832a895c6e8
jose@proint.com <jose@proint.com>
2024-09-09 21:44:05 +0000

    Edited homepage.deepsearch

--- client_code/homepage/deepsearch/__init__.py
+++ client_code/homepage/deepsearch/__init__.py
@@ -283,7 +283,10 @@ class deepsearch(deepsearchTemplate):
       else:
         sqlStr=f" {sqlStr} and ubicacion like '{ubiGlob}'"
         comandoStr=f" {comandoStr} y la Ubicacion comienze con '{ubiGlob}'"
-        anvil.alert(sqlStr)
+        anvil.alert(f"aquivoy 1: {sqlStr}")
+      self.lbl_sql.text=""
+      self.txt_comando.text=""
+      anvil.alert(f"aquivoy 2: {sqlStr}")
       self.lbl_sql.text = sqlStr
       self.txt_comando.text = comandoStr
       
