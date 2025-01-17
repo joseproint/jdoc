4025f5f993b1b03bd73e263dfc2006de69530cda
jose@proint.com <jose@proint.com>
2025-01-17 19:48:51 +0000

    Edited homepage.expediente

--- client_code/homepage/expediente/__init__.py
+++ client_code/homepage/expediente/__init__.py
@@ -191,8 +191,9 @@ class expediente(expedienteTemplate):
     tipotrans='TRANSFERENCIA'
     numtrans = self.txt_lat.text
     firma = anvil.server.call('get_foto',tipotrans,numtrans)
+    foto = firma.encode('utf-8')
     if firma:
-      self.image_2.source=firma
+      self.image_2.source=foto
     else:
       alert("foto no es válida..")
 
