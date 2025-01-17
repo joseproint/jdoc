4532b1b29b41a8351eb1f8c52623ce257e8f76d8
jose@proint.com <jose@proint.com>
2025-01-17 15:47:48 +0000

    Edited jDoc

--- server_code/jDoc.py
+++ server_code/jDoc.py
@@ -1003,7 +1003,8 @@ def transfiereExp(fecha,codExpediente,empRecibe,empEntrega,notas,tipotrans,numTr
   firmaOriginal=firma.get_bytes()
   #insertaFirma(tipotrans,numtrans,fotoProcesada,cType,fotoProcesada)
   #insertaFirma(tipotrans,numtrans,fotoProcesada,cType,fotoProcesada)
-  insertaFirma(tipotrans,numtrans,fotoProcesada,cType,firma)
+  #insertaFirma(tipotrans,numtrans,fotoProcesada,cType,firma)
+  insertaFirma(tipotrans,numtrans,fotoProcesada,cType,fotoBytes)
   #=========== fin manejo de la firma =================
   if tipotrans=='ACUSERECIBO' or tipotrans=='DEVOLUCION':
     numRecibo=numtrans
