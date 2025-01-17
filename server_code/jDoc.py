2d77d195d405ac0b84a2d906488cd7ec32586f52
jose@proint.com <jose@proint.com>
2025-01-17 19:52:22 +0000

    Edited jDoc

--- server_code/jDoc.py
+++ server_code/jDoc.py
@@ -434,7 +434,8 @@ def get_foto(tipotrans,numtrans):
       if foto is not None:
         #newfoto=anvil.BlobMedia("image/png",foto)
         newfoto=anvil.BlobMedia("image/jpeg",foto)
-    return newfoto
+    #return newfoto
+    return foto
 
 @anvil.server.callable
 def get_ponchesXRango(fini,ffin,email):
