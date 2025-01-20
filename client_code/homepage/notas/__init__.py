c0d2fba851bac55edaf7edf207aa0a44ff84016e
jose@proint.com <jose@proint.com>
2025-01-20 21:43:23 +0000

    Edited homepage.notas

--- client_code/homepage/notas/__init__.py
+++ client_code/homepage/notas/__init__.py
@@ -11,28 +11,30 @@ class notas(notasTemplate):
     self.init_components(**properties)
     self.lbl_notas.text=item['notas']
 
-    tipotrans='TRANSFERENCIA'
+    #tipotrans='TRANSFERENCIA'
+    tipotrans= item['tipotrans']
     numtrans = item['numtrans']
     columna = 'firma'
-    firma = anvil.server.call('get_foto',tipotrans,numtrans,columna)
-    if firma:
-      #self.image_2.source=firma
-      mime_type="image/jpg"
-      binary_data = base64.b64decode(firma)
-      media_Object=BlobMedia(mime_type, binary_data)
-      self.image_firma.source=media_Object
-    else:
-      alert("Imagen firma no es válida..")
+    if tipotrans=='TRANSFERENCIA':
+      firma = anvil.server.call('get_foto',tipotrans,numtrans,columna)
+      if firma:
+        #self.image_2.source=firma
+        mime_type="image/jpg"
+        binary_data = base64.b64decode(firma)
+        media_Object=BlobMedia(mime_type, binary_data)
+        self.image_firma.source=media_Object
+      else:
+        alert("Imagen firma no es válida..")
 
     #Ahora leo la foto de la cedula de identidad
-    columna = 'cedula'
-    cedula = anvil.server.call('get_foto',tipotrans,numtrans,columna)
-    if cedula:
-      #self.image_2.source=firma
-      mime_type="image/jpg"
-      binary_data = base64.b64decode(cedula)
-      media_Object=BlobMedia(mime_type, binary_data)
-      self.image_cedula.source=media_Object
-    else:
-      alert("Imagen Cedula de Identidad no es válida..")    
-    # Any code you write here will run before the form opens.
+    if tipotrans=='TRANSFERENCIA':
+      columna = 'cedula'
+      cedula = anvil.server.call('get_foto',tipotrans,numtrans,columna)
+      if cedula:
+        #self.image_2.source=firma
+        mime_type="image/jpg"
+        binary_data = base64.b64decode(cedula)
+        media_Object=BlobMedia(mime_type, binary_data)
+        self.image_cedula.source=media_Object
+      else:
+        alert("Imagen Cedula de Identidad no es válida..")    
